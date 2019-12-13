6daab244646a5a5c9f0410b1a2648d65e9e9713b
chibieee <gregneo21@gmail.com>
2019-12-12 20:33:22 -0500

    fix slick width problem

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var navBar = document.getElementsByTagName("nav");
-    console.log(navBar);
+
     window.addEventListener("scroll", function() {
         if (window.pageYOffset > 0) {
             navBar[0].classList.add("scrolled");
