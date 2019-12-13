7d42c4bc88e0e6c2c8eab8663b92807a241fa254
chibieee <gregneo21@gmail.com>
2019-12-12 19:57:43 -0500

    added slick

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var navBar = document.getElementsByTagName('nav');
+document.addEventListener("DOMContentLoaded", function() {
+    var navBar = document.getElementsByTagName("nav");
     console.log(navBar);
-    window.addEventListener('scroll', function() {
-        if(window.pageYOffset > 0) {
-            navBar[0].classList.add('scrolled');
+    window.addEventListener("scroll", function() {
+        if (window.pageYOffset > 0) {
+            navBar[0].classList.add("scrolled");
         } else {
-            navBar[0].classList.remove('scrolled');
+            navBar[0].classList.remove("scrolled");
         }
+    });
+    $(".pro-work-slider").slick({
+        arrows: true,
+        dots: true,
+        speed: 1500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        nextArrow: '<i class="fas fa-chevron-right"></i>',
+        prevArrow: '<i class="fas fa-chevron-left"></i>'
     });
 });
